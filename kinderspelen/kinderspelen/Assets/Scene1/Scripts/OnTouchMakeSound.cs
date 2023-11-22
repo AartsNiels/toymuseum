61b4be02c22c0b70743f59dececa18894fb8115f
AartsNiels <114395737+AartsNiels@users.noreply.github.com>
2023-11-22 11:14:17 +0100

    Turd poking

---
+++ kinderspelen/kinderspelen/Assets/Scene1/Scripts/OnTouchMakeSound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTouchMakeSound : MonoBehaviour
+{
+    public AudioClip Sound; // Geluid dat afgespeeld moet worden
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+
+        if (other.gameObject.tag == "Stick")
+        {
+            AudioSource.PlayClipAtPoint(Sound, transform.position);
+        }
+    }
+}
