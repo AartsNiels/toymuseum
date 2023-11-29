79152acbfe28603e2a6dc75a634e3ff24ea76e79
AartsNiels <114395737+AartsNiels@users.noreply.github.com>
2023-11-29 14:40:57 +0100

    Fade in scene1
    + can only teleport to scene2 with a interaction instead of a collision

---
+++ kinderspelen/kinderspelen/Assets/Scene1/Scripts/Game_manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_manager : MonoBehaviour
+{
+    public GameObject CanvasFade;
+    private bool fadeout = false;
+
+    void Start()
+    {
+        CanvasFade.GetComponent<FadeCanvas>().StartFadeOut();
+    }
+
+    void Update()
+    {
+        if (fadeout)
+        {
+            if (CanvasFade.GetComponent<FadeCanvas>().GetComponent<CanvasGroup>().alpha == 1f)
+            {
+                SceneManager.LoadScene("Scene2");
+            }
+        }
+    }
+}
